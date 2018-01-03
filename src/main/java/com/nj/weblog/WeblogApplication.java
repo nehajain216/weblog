b222577b6a5ecdad430951650d73a005bb5074dc
Neha Jain <neha.jain216@gmail.com>
2018-01-03 08:40:53 +0530

    Initial commit.

---
+++ src/main/java/com/nj/weblog/WeblogApplication.java
@@ -0,0 +1,12 @@
+package com.nj.weblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblogApplication.class, args);
+	}
+}
