d79ec2d6dfb91460b2acb891675b0dc1cb956880
Neha Jain <neha.jain216@gmail.com>
2018-01-05 10:37:48 +0530

    Completed issue #1 - Design Database #14 - Post Page: Display full content of post along with comments #19 - Right Sidebar: Categories #20 - Right Sidebar: Tags #23 - Posts by Category: Display posts which are associated with Selected Category #24 - Posts by Tag: Display posts which are associated with Selected Tag

--- src/main/java/com/nj/weblog/WeblogApplication.java
+++ src/main/java/com/nj/weblog/WeblogApplication.java
@@ -2,11 +2,20 @@ package com.nj.weblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@EntityScan(
+        basePackageClasses = {WeblogApplication.class, Jsr310JpaConverters.class}
+)
 @SpringBootApplication
 public class WeblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeblogApplication.class, args);
 	}
+	
 }
--- src/main/java/com/nj/weblog/web/controllers/HomeController.java
+++ src/main/java/com/nj/weblog/web/controllers/HomeController.java
@@ -1,14 +1,81 @@
 package com.nj.weblog.web.controllers;
 
+import java.util.List;
+import java.util.Set;
+
+import org.junit.experimental.categories.Categories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.nj.weblog.entities.Category;
+import com.nj.weblog.entities.Post;
+import com.nj.weblog.entities.Tag;
+import com.nj.weblog.repositories.CategoryRepository;
+import com.nj.weblog.repositories.PostRepository;
+import com.nj.weblog.repositories.TagRepository;
 
 @Controller
-public class HomeController {
-
+public class HomeController 
+{
+	private final PostRepository postRepository;
+	private final CategoryRepository categoryRepository;
+	private final TagRepository tagRepository;
+	
+	@Autowired
+	public HomeController(PostRepository postRepository, CategoryRepository categoryRepository, TagRepository tagRepository) {
+		super();
+		this.postRepository = postRepository;
+		this.categoryRepository = categoryRepository;
+		this.tagRepository = tagRepository;
+	}
+	
 	@GetMapping("/")
-	public String index()
+	public String getAllPosts(Model model)
 	{
+		List<Post> posts = postRepository.findAll();
+		model.addAttribute("allPosts", posts);
 		return "index";
+	}
+	
+	@GetMapping("/posts/{id}")
+	public String getPostById(@PathVariable("id") Integer id, Model model)
+	{
+		Post post = postRepository.findOne(id);
+		model.addAttribute("post",post);
+		return "post";
+	}
+	
+	@GetMapping("/category/{categoryname}")
+	public String getByCategory(@PathVariable("categoryname") String categoryName, Model model)
+	{
+		Category category = categoryRepository.findByName(categoryName);
+		model.addAttribute("category",category);
+		return "postsbycategory";
+	}
+	
+	@GetMapping("/tag/{tagname}")
+	public String getByTag(@PathVariable("tagname") String tagName, Model model)
+	{
+		Tag tag = tagRepository.findByName(tagName);
+		model.addAttribute("tag",tag);
+		return "postsbytag";
+	}
+	
+	@ModelAttribute(name="categories")
+	public List<Category> allCategories()
+	{
+		List<Category> categories = categoryRepository.findAll();
+		return categories;
+	}
+	
+	@ModelAttribute(name="tags")
+	public List<Tag> allTags()
+	{
+		List<Tag> tags = tagRepository.findAll();
+		return tags;
 	}
 }
