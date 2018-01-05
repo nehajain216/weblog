fca0514b5bd7a40a18b5148635ed0c9db4c78335
Neha Jain <neha.jain216@gmail.com>
2018-01-05 18:31:13 +0530

    Complete issue #12 and #13
    #12:Home Page: List N number of posts per page with short description
    And
    #13: Home Page: Should support pagination with First, Previous, Next,
    Last links

--- src/main/java/com/nj/weblog/web/controllers/HomeController.java
+++ src/main/java/com/nj/weblog/web/controllers/HomeController.java
@@ -1,15 +1,17 @@
 package com.nj.weblog.web.controllers;
 
-import java.util.List;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
 
-import org.junit.experimental.categories.Categories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nj.weblog.entities.Category;
 import com.nj.weblog.entities.Post;
@@ -34,48 +36,79 @@ public class HomeController
 	}
 	
 	@GetMapping("/")
-	public String getAllPosts(Model model)
+	public String getAllPosts(Model model, @RequestParam(defaultValue="0") int page)
 	{
-		List<Post> posts = postRepository.findAll();
-		model.addAttribute("allPosts", posts);
+		Page<Post> allPosts = postRepository.findAll(new PageRequest(page, 2));
+		model.addAttribute("allPosts", allPosts);
+		model.addAttribute("currentPage", page);
 		return "index";
 	}
 	
 	@GetMapping("/posts/{id}")
 	public String getPostById(@PathVariable("id") Integer id, Model model)
 	{
-		Post post = postRepository.findOne(id);
-		model.addAttribute("post",post);
+		model.addAttribute("post",postRepository.findOne(id));
 		return "post";
 	}
 	
 	@GetMapping("/category/{categoryname}")
 	public String getByCategory(@PathVariable("categoryname") String categoryName, Model model)
 	{
-		Category category = categoryRepository.findByName(categoryName);
-		model.addAttribute("category",category);
+		model.addAttribute("category",categoryRepository.findByName(categoryName));
 		return "postsbycategory";
 	}
 	
 	@GetMapping("/tag/{tagname}")
 	public String getByTag(@PathVariable("tagname") String tagName, Model model)
 	{
-		Tag tag = tagRepository.findByName(tagName);
-		model.addAttribute("tag",tag);
+		model.addAttribute("tag",tagRepository.findByName(tagName));
 		return "postsbytag";
 	}
 	
 	@ModelAttribute(name="categories")
 	public List<Category> allCategories()
 	{
-		List<Category> categories = categoryRepository.findAll();
-		return categories;
+		return categoryRepository.findAll();
 	}
 	
 	@ModelAttribute(name="tags")
 	public List<Tag> allTags()
 	{
-		List<Tag> tags = tagRepository.findAll();
-		return tags;
+		return tagRepository.findAll();
 	}
+	
+	@ModelAttribute(name="archival")
+	public Model archival(Model model)
+	{
+		List<Post> allPost = postRepository.findAll();
+		List<LocalDate> dates = new ArrayList<>();
+		for (Post post : allPost) {
+			dates.add(post.getCreatedOn());
+		}
+		
+		int minYear = Collections.min(dates).getYear();
+		int maxYear = Collections.max(dates).getYear();
+		Map<String,List<Integer>> months = new HashMap<>();
+		
+			for (LocalDate date : dates) 
+			{
+				for (int i = minYear; i <= maxYear; i++) 
+				{
+					if(date.getYear() == i)
+					{
+						date.getMonth();
+					}
+				}
+				
+			}
+		
+		
+		
+		/*model.addAttribute("minYear", minYear);
+		model.addAttribute("maxYear", maxYear);*/
+		
+		return model;
+		
+	}
+	
 }
